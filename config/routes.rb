8c319ad89228816f55d62b0b1fe5f94a93ac6486
Ben S <bendev@Bens-MacBook-Air.local>
2020-02-25 15:46:24 -0500

    block add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/show'
+  get 'home/show'
+
+  resources :pages, only: [:show]
+
+  root 'home#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
