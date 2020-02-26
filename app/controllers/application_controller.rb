a3170273729706bde4efc80f98264294739cbac3
Ben S <bendev@Bens-MacBook-Air.local>
2020-02-25 23:27:00 -0500

    layout tweak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :getPageNav
+
+  protect_from_forgery with: :exception
+
+
+
+  def getPageNav
+    @pageNav = Page.where("menu display = true && is_published = true").order(order: :asc)
+  end
 end
