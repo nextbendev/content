6a3c8ba9a1aba41ef49ddc72c4ef2700d0868fa9
Ben S <bendev@Bens-MacBook-Air.local>
2020-02-28 19:52:31 -0500

    jumbotron work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :getPageNav
+  before_action :getPageNav, :getJumbotron, :getBlocks
 
   # protect_from_forgery with: :exception
 
   def getPageNav
     @pageNav = Page.where("menu_display = true and is_published = true").order(order: :asc)
   end
-  #
-  # def getJumbotron
-  #   @Jumbotron = Block.where
-  # end
+
+   def getJumbotron
+     @jumbotron = Block.where("position = 'jumbotron'")
+   end
+
+   def getBlocks
+     @blocks = Block.where("position ='block'").order(order: :asc)
+   end
 
 end
